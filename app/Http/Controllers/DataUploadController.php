010999ae71c2153bd743c1c363872afea9927992
AlexAsofiei <95917292+AlexAsofiei@users.noreply.github.com>
2025-03-05 15:38:04 +0000

    backend test 100% done

--- app/Http/Controllers/DataUploadController.php
+++ app/Http/Controllers/DataUploadController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use App\Models\MetricValue;
+use Illuminate\Support\Facades\Validator;
+use App\Models\Metric;
 
 class DataUploadController extends Controller
 {
@@ -11,6 +14,52 @@ class DataUploadController extends Controller
     {
         $file = $request->file('file');
         $contents = file_get_contents($file->getRealPath());
+
+        $lines = explode(PHP_EOL, $contents);
+        $data = [];
+        $metrics = [];
+        $csvHeaders = ['achieved_at', 'metric_key', 'external_id', 'value'];
+        $lines = array_slice($lines, 1);
+
+        foreach ($lines as $line) {
+            //ignore empty lines
+            if (empty($line)) {
+                continue;
+            }
+
+            //transform every line to array
+            $row = str_getcsv($line);
+            $mappedRow = array_combine($csvHeaders, $row);
+
+            $validator = Validator::make($mappedRow, [
+                'achieved_at' => 'required',
+                'metric_key' => 'required',
+                'external_id' => 'required',
+                'value' => 'required'
+            ]);
+
+            if ($validator->fails()) {
+                return response()->json(['success' => false, 'message' => 'Invalid data']);
+            }
+
+            //check if metric_key exists in Metric
+            $metricKey = $mappedRow['metric_key'];
+
+            if (!isset($metrics[$metricKey])) {
+                //if doesn't exist, we'll add it
+                $metric = Metric::firstOrCreate(['metric_key' => $metricKey]);
+
+                //save the id for further use
+                $metrics[$metricKey] = $metric->id;
+            }
+
+            $mappedRow['metric_id'] = $metrics[$metricKey];
+
+            unset($mappedRow['metric_key']);
+
+            $data[] = $mappedRow;
+        }
+        MetricValue::insert($data);
 
         return response()->json([
             'success' => true,
--- app/Http/Controllers/MetricStatsController.php
+++ app/Http/Controllers/MetricStatsController.php
@@ -4,14 +4,29 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use App\Models\MetricValue;
 
 class MetricStatsController extends Controller
 {
     public function userDaily(Request $request): JsonResponse
     {
-        $output = [];
+        // fetch params
+        $externalId = $request->query('external_id');
+        $metricKey = $request->query('metric_key');
+        $date = $request->query('date', '2024-12-18'); // defalut value for date
 
-        $output[] = ['date' => '2024-12-18', 'value' => 37];
+        // verify all params
+        if (!$externalId || !$metricKey) {
+            return response()->json([
+                'success' => false,
+                'message' => 'The required parameters (external_id, metric_key) are missing.'
+            ], 400);
+        }
+
+        
+        $output = [];
+        
+        $output[] = ['date' => $date, 'value' => 102]; 
 
         return response()->json($output);
     }
